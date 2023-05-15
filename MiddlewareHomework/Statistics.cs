f39e0063b804dad5732618194dc40ac1d3d39aaa
Admin <Admin@LENOVO>
2023-05-15 14:47:41 +0600

    Добавьте файлы проекта.

---
+++ MiddlewareHomework/Statistics.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiddlewareHomework
+{
+    public class Statistics
+    {
+        private readonly RequestDelegate _request;
+
+        public Statistics(RequestDelegate request)
+        {
+            request = _request;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            int requestNumber = 0;
+
+            if (context.Request.Cookies.ContainsKey("requestNumber"))
+            {
+                try
+                {
+                    requestNumber = int.Parse(context.Request.Cookies["requestNumber"]);
+                }
+                catch (Exception e)
+                {
+                    throw new Exception(e.Message);
+                }
+            }
+            else
+            {
+                context.Response.Cookies.Append("requestNumber", requestNumber.ToString());
+            }
+
+            requestNumber = requestNumber + 1;  
+
+            try
+            {
+                //context.Request.Cookies["requestNumber"] = requestNumber.ToString();
+
+                context.Response.Cookies.Append("requestNumber", requestNumber.ToString());
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+
+    public static class ContentMiddlewareExtension
+    {
+        public static IApplicationBuilder stat(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<Statistics>();
+        }
+    }
+}
